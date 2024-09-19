0b6bcf248a709aff98839872486d6d3c3a452f19
AnnaGustafson2 <161380808+AnnaGustafson2@users.noreply.github.com>
2024-09-19 15:54:04 -0400

    updated building models

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,6 +6,18 @@ import model from '../script';
 const MainPage = () => {
     const [guess, setGuess] = useState('');
     const [guesses, setGuesses] = useState([]);
+    const woods = {"name": "Woods", "xCoord": 38.98520825094225, "yCoord": -76.94178927335166};
+    const maryMount = {"name": "Mary Mount", "xCoord": 38.9851333833028, "yCoord": -76.94086354821499};
+    const dorchester = {"name": "Dorchester", "xCoord": 38.98684874201056, "yCoord": -76.9461386886941};
+    const pyonChen = {"name": "Pyon Chen", "xCoord": 38.99217621364386, "yCoord": -76.94490983773476};
+
+    //all building variables defined
+    const buildings = [this.woods, this.maryMount, this.dorchester, this.pyonChen];
+    //array of all buildings
+    let list = [];
+    //list for guesses, ordered closest to furthests
+    let answer = this.pyonChen;
+    let guessed = false;
 
     const handleInputChange = (event) => {
         setGuess(event.target.value);
@@ -28,6 +40,22 @@ const MainPage = () => {
         ));
     };
 
+    const displayOutput = () => {
+        if (guessed == true) {
+            console.log("end game");
+            document.getElementById('output').textContent = `You Won!`;
+        } else {
+            document.getElementById('output').textContent = `Closests:`;
+            list.sort((a, b) => calculateDistance(a, answer) - calculateDistance(b, answer));
+            for (let i = 0; i < list.length; i++) {
+                if (i != 0) {
+                    document.getElementById('output').textContent += ',';
+                }
+                document.getElementById('output').textContent += ' ' + list[i].name;
+            }
+        }  
+    }
+
     return (
         <div id="body">
             <div id="left-container">
@@ -46,7 +74,7 @@ const MainPage = () => {
                     <button type="submit">Enter</button>
                 </form>
                 <p id="output">Your Guesses:</p>
-                {printGuesses()}
+                {displayOutput()}
             </div>
             <div id="right-container">
                 <img id="image" src={campus} alt="UMD Campus"/>
